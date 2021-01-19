dd0a55721525061ed29501d5880820c0b8d5b52c
karolnowak <karol.nowak2298@gmail.com>
2021-01-19 19:12:09 +0100

    #5 User authentication and login done

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\CommentsController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,18 +16,10 @@ use App\Http\Controllers\CommentsController;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 });
-/*Route::get('/comments',[CommentsController::class,'index']);
-*/
-Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/comments', [CommentsController::class, 'index'])->name('comments');
-Route::get('/create', [CommentsController::class, 'create'])->name('create');
-Route::post('/create', [CommentsController::class, 'store'])->name('store');
-Route::get('/delete/{id}',[CommentsController::class,'destroy'])->name('delete');
-Route::get('/edit/{id}', [CommentsController::class,'edit'])->name('edit');
-Route::put('{id}', [CommentsController::class,'update'])->name('update');
+Route::post("/login",[UserController::class,'login']);
+Route::get("/",[ProductController::class,'index']);
 
