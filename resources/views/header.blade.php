fab113ba16810650f5a82fa6a49a98b0d35dce0e
karolnowak <karol.nowak2298@gmail.com>
2021-01-21 19:21:53 +0100

    #15 Cart listing

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -30,7 +30,7 @@ if(Session::has('user')){
         <button type="submit" class="btn btn-default">Wyszukaj</button>
       </form>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Koszyk({{$total}})</a></li>
+        <li><a href="/cartlist">Koszyk({{$total}})</a></li>
         @if(Session::has('user'))
         <li class="dropdown">
         <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
